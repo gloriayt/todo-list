5abfdaeb0a5317b589e62cd91ce32f88925cbda5
gloriayt <gloriaytai@gmail.com>
2020-08-07 22:37:48 +1000

    Updates

---
+++ precache-manifest.8137501bfffbcd93ba6ba462b056b9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee7383a386696ef8f57df15cc560c9d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "eebf96ed25440cd71d9f",
+    "url": "/todo-list/static/css/main.93f37032.chunk.css"
+  },
+  {
+    "revision": "cade0a0903d3eb5baed3",
+    "url": "/todo-list/static/js/2.5498fc43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.5498fc43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebf96ed25440cd71d9f",
+    "url": "/todo-list/static/js/main.0d7eff6a.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
